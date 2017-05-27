5b6e22d903b8362f949d1ce7cde42ba6d61f4d61
androsh <apanasiuk00@gmail.com>
2017-05-27 10:42:42 +0100

    test: fix DOM access

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,24 +31,24 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         it('URL is defined and not empty', function() {
+        it('URL is defined and not empty', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.url).toBeDefined();
                 expect(feed.url.length).not.toBe(0);
             });
-         });
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-         it('Name is defined and not empty', function() {
+        it('Name is defined and not empty', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.name).toBeDefined();
                 expect(feed.name.length).not.toBe(0);
             });
-         });
+        });
     });
 
 
@@ -59,21 +59,21 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-         it('Menu hidden by default', function() {
+        it('Menu hidden by default', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
-         });
+        });
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-          it('Menu visibility toggled when clicked', function() {
+        it('Menu visibility toggled when clicked', function() {
             $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).toBe(false);
 
             $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).toBe(true);
-          });
+        });
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
@@ -99,21 +99,21 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-         var feed;
+        var feed;
 
-         beforeEach(function(done) {
+        beforeEach(function(done) {
             loadFeed(1, function() {
-                feed = $('.feed').html;
+                feed = $('.feed').html();
                 done();
             });
-         });
+        });
 
-         it('check if new feed is loaded', function(done) {
+        it('check if new feed is loaded', function(done) {
             loadFeed(0, function() {
                 expect($('.feed').html()).not.toEqual(feed);
                 done();
             });
-         });
+        });
 
     });
 }());
